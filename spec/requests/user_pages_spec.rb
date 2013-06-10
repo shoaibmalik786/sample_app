2505572e231ad1984c6462748e0db1024ddf8aac
Shoaib Malik <shoaib.malik@addvalsolutions.com>
2013-06-10 15:32:01 +0530

    make a basic user model with secure password

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,13 +1,38 @@
 require 'spec_helper'
 
-describe "User pages" do
+describe User do
 
-  subject { page }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
-  describe "signup page" do
-    before { visit signup_path }
+  subject { @user }
+  .
+  .
+  .
+  it { should respond_to(:authenticate) }
+  .
+  .
+  .
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
 
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
   end
 end
